e8dcd2728b17ab0555899a7682410c1d6d27bde3
Prashant Singh <prashantsingh896@gmail.com>
2023-01-05 20:05:28 +0530

    add a layout

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 const express = require('express');
 const app = express();
 const port = 8000;
+//require for layouts
+var expressLayouts = require('express-ejs-layouts');
 
-//use express router
-app.use('/', require('./routes'));
 
 //set up view engine
 app.set('view engine', 'ejs');
 app.set('views', './views');
+
+//use ejs layouts
+app.use(expressLayouts);
+
+
+//use express router
+app.use('/', require('./routes'));
+
 
 app.listen(port, function(err){
     if(err){
