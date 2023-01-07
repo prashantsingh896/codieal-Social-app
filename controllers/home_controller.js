8fd67ee29ec13925d70a877db0de576e82c96891
Prashant Singh <prashantsingh896@gmail.com>
2023-01-07 18:47:34 +0530

    render post to home and show author

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,7 +1,24 @@
+const Post = require('../models/post');
+
 module.exports.home = function(req,res){
-    console.log(req.cookies);
-    res.cookie('user',25);
-    return res.render('home',{
-        title:'Home'
+
+    // Post.find({}, function(err, posts){
+    //     if(err){
+    //         console.error.bind(`Error in loading posts from DB ${err}`);
+    //     }
+    //     else{
+    //         return res.render('home', {
+    //             title: "codeial | Home",
+    //             posts: posts
+    //         });
+    //     }
+    // });
+
+    //populate the user of all posts
+    Post.find({}).populate('user').exec(function(err, posts){
+        return res.render('home',{
+            title: "Codeial | Home",
+            posts: posts
+        });
     });
-}
+};
