f7b9637b6532381a6f74c1f1908d3ad46f0b37a5
Prashant Singh <prashantsingh896@gmail.com>
2023-01-08 01:04:22 +0530

    add user detail update option in profile

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -4,11 +4,21 @@ module.exports.profile = function (req, res) {
     User.findById(req.params.id, function(err,user){
         return res.render('user_profile',{
             title: 'User Profile',
-            user: user
+            profile_user: user
         });
     });
     
 }
+
+module.exports.update = function(req,res){
+    if(req.user.id == req.params.id){
+        User.findByIdAndUpdate(req.params.id,req.body,function(err,user){
+            return res.redirect('back');
+    });
+    }else{
+        return res.status(401).send('Unauthorized');
+    }
+};
 
 //render the sign Up page
 module.exports.signUp = function (req, res) {
