0e4d125e42361ce39b01075766e5b2da2d524cbe
Prashant Singh <prashantsingh896@gmail.com>
2023-01-09 22:30:46 +0530

    use multer and add option to upload pic on profile page

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -10,14 +10,42 @@ module.exports.profile = function (req, res) {
     
 }
 
-module.exports.update = function(req,res){
+module.exports.update = async function(req,res){
+    // if(req.user.id == req.params.id){
+    //     User.findByIdAndUpdate(req.params.id,req.body,function(err,user){
+    //         return res.redirect('back');
+    // });
+    // }else{
+    //     return res.status(401).send('Unauthorized');
+    // }
+
     if(req.user.id == req.params.id){
-        User.findByIdAndUpdate(req.params.id,req.body,function(err,user){
+        try{
+            let user = await User.findByIdAndUpdate(req.params.id);
+            User.uploadedAvatar(req,res,function(err){
+                if(err){console.log('***Multer Error',err); return;}
+
+                user.name = req.body.name;
+                user.email = req.body.email;
+                if(req.file){
+                    //this is saving the path of the uploaded file into avatar field in user
+                    user.avatar = User.avatarPath + '/' + req.file.filename;
+                }
+                user.save();
+                return res.redirect('back');
+            })
+
+        }
+        catch(e){
+            req.flash('error');
             return res.redirect('back');
-    });
+        }
+
     }else{
+        req.flash('error','Unauthorized!');
         return res.status(401).send('Unauthorized');
     }
+
 };
 
 //render the sign Up page
