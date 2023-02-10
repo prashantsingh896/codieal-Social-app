6e162927272c8806c371dbee19eb7720d2ba6d93
Prashant Singh <prashantsingh896@gmail.com>
2023-02-10 17:41:13 +0530

    install dotenv and create production env variables

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
+require('dotenv').config()
 const env = require('./config/environment');
 const app = express();
 const port = 8000;
